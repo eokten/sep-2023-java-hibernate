f8b5e8c0ae0b882d7cd907fc4adc425847b9164e
EOkten <eduardokten@gmail.com>
2024-04-26 22:26:04 +0300

    Initial commit

---
+++ src/main/java/org/okten/Product.java
@@ -0,0 +1,29 @@
+package org.okten;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "price") // optional
+    private double price;
+
+    @Column(name = "current_discount") // required
+    private double currentDiscount;
+}
